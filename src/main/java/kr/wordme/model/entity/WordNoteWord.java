3011f7b21f2aa6acf5b683d0f05326a7152d415d
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-11 04:03:47 +0900

    Feat: word note related entity implement
    단어장 관련 엔티티 구현,
    단어장 엔티티 구현
    단어장 단어 엔티티 구현
    단어장 단어 의미 엔티티 구현
    단어장 단어 태그 엔티티 구현

---
+++ src/main/java/kr/wordme/model/entity/WordNoteWord.java
@@ -0,0 +1,43 @@
+package kr.wordme.model.entity;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "word_note_word")
+@NoArgsConstructor
+@Getter
+public class WordNoteWord {
+
+	@Id
+	private UUID id;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "word_note_id")
+	private WordNote wordNote;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "word_id")
+	private Word word;
+
+	@Column(name = "created_at", updatable = false)
+	@CreationTimestamp
+	private Timestamp createdAt;
+
+	@OneToMany(mappedBy = "wordNoteWord", fetch = FetchType.LAZY)
+	private List<WordNoteWordMeaning> wordNoteWordMeanings;
+}
