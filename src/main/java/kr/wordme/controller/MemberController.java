bdc353dbaa01963ae274a24767cfd70a05ce6759
keyy1315 <lovelina1315@gmail.com>
2024-10-18 04:28:19 +0900

    Feat : implement logout process

--- src/main/java/kr/wordme/controller/MemberController.java
+++ src/main/java/kr/wordme/controller/MemberController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.wordme.common.CustomResponseMessage;
+import kr.wordme.filter.JwtFilter;
 import kr.wordme.model.dto.JwtDTO;
 import kr.wordme.model.dto.request.SignupRequestDTO;
 import kr.wordme.model.dto.response.MemberInfoResponseDTO;
@@ -14,8 +15,10 @@ import kr.wordme.service.EmailService;
 import kr.wordme.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -31,18 +34,17 @@ public class MemberController {
      * 1. 이메일 검증 (30분 유효한 임시 토큰으로 검증)
      * 2. 이메일 검증 결과(isVerify), 비밀번호 등등 담긴 signupRequestDTO 로 회원가입 진행
      * 3. 검증 결과가 true 일 때만 회원가입 완료
-     * <p>
+     * <p/>
      * 4. 이메일, 비밀번호로 로그인 진행 access, refresh token cookie 에 담고 index redirect
-     * 5. cookie 에 담긴 token 들 request Header 에 담아서 /info endpoint 로 정보 요청
      * # oAuth2 회원가입 & 로그인
      * 1. DB에 같은 이메일 있는지 확인 후 DB에 저장
      * 2. 회원가입 / 로그인 성공 시 token cookie 에 담고 index redirect
      * 3. Header 에 token 담고 정보 요청
      **/
 
-
     private final MemberService memberService;
     private final EmailService mailService;
+    private final JwtFilter jwtFilter;
 
     @PostMapping("/sign-up")
     public ResponseEntity<Object> signUp(@RequestBody SignupRequestDTO signupRequestDTO) {
@@ -65,7 +67,11 @@ public class MemberController {
 
     @GetMapping("/info")
     public ResponseEntity<Object> info(@AuthenticationPrincipal Member member) {
-        MemberInfoResponseDTO infoDTO = MemberInfoResponseDTO.of(member.getNickname());
+        MemberInfoResponseDTO infoDTO = null;
+        if (!ObjectUtils.isEmpty(member)) {
+            infoDTO = MemberInfoResponseDTO.from(member);
+        }
+//        filter 에서 쿠키 확인 후 쿠키가 없다면 null 로 응답
         return ResponseEntity.ok().body(infoDTO);
     }
 
@@ -73,10 +79,6 @@ public class MemberController {
     public ResponseEntity<Object> sendEmail(@RequestParam("email") String email) throws MessagingException {
         memberService.duplicatedEmail(email); //메일 중복
         mailService.sendEmail(email); //검증 링크 전송
-
-//        1. 이메일 인증 버튼 클릭 시 이메일로 리액트 리다이렉트 링크 발송
-//        2. 리액트에서 /verify 엔드포인트로 유효한 토큰인지 검증
-//        3. result 가 true 면 인증되었다는 표시와 함께 회원가입 절차 진행
 
 //        temp : 링크 클릭하면 boolean 담긴 dto 반환하도록 구현
         return ResponseEntity.ok().body(new CustomResponseMessage("success to send email"));
@@ -88,5 +90,14 @@ public class MemberController {
         VerificationEmailResponseDTO responseDTO = VerificationEmailResponseDTO.create(email, verify);
         return ResponseEntity.ok().body(responseDTO);
 //        이메일 토큰 검증 후 유효한 토큰이면 true 반환
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Object> logout(HttpServletResponse resp) {
+        Cookie[] deleteCookies = jwtFilter.cookieDelete();
+        for (Cookie deleteCookie : deleteCookies) {
+            resp.addCookie(deleteCookie);
+        }
+        return ResponseEntity.ok().body(new CustomResponseMessage("logout success"));
     }
 }
