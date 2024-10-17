b1e60961561d5db560d1151a4595a22f0d6f43e2
keyy1315 <lovelina1315@gmail.com>
2024-10-17 14:32:50 +0900

    Feat: Implement Custom responseEntity message & Custom Exception Code

--- src/main/java/kr/wordme/exception/ErrorCode.java
+++ src/main/java/kr/wordme/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     NOT_EXIST_USER(HttpStatus.NOT_FOUND, "non-existent user"),
-    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "duplicated email");
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "duplicated email"),
+    FAIL_SEND_EMAIL(HttpStatus.BAD_REQUEST,"fail to send email");
 
     private final HttpStatus status;
     private final String message;
