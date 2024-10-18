92f8069335f08d2b23ca75effd14591e500ac036
keyy1315 <lovelina1315@gmail.com>
2024-10-18 14:56:28 +0900

    Feat : implement DTO's static factory method

--- src/main/java/kr/wordme/model/dto/request/SignupRequestDTO.java
+++ src/main/java/kr/wordme/model/dto/request/SignupRequestDTO.java
@@ -1,5 +1,6 @@
 package kr.wordme.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,10 +8,14 @@ import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequestDTO {
-    private UUID id;
     private String email;
     private String password;
     private String nickname;
     private boolean isVerify;
+
+    public static SignupRequestDTO create(String mail, String password, String nickname, boolean isVerify) {
+        return new SignupRequestDTO(mail, password, nickname,isVerify);
+    }
 }
