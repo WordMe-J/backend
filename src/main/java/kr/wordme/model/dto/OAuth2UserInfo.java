d9319f0b419a928547a8cde0880028715173940e
IY E <iye@IYui-MacBookPro.local>
2024-11-05 15:56:01 +0900

    update develop branch

--- src/main/java/kr/wordme/model/dto/OAuth2UserInfo.java
+++ src/main/java/kr/wordme/model/dto/OAuth2UserInfo.java
@@ -17,7 +17,8 @@ public class OAuth2UserInfo {
     private String nickname;
     private String provider;
 
-    public static OAuth2UserInfo of(String provider, Map<String, Object> attributes) throws IllegalAccessException {
+    public static OAuth2UserInfo of(String provider, Map<String, Object> attributes)
+            throws IllegalAccessException {
         return switch (provider) {
             case "google" -> ofGoogle(attributes);
             case "naver" -> ofNaver(attributes);
@@ -26,30 +27,20 @@ public class OAuth2UserInfo {
     }
 
     private static OAuth2UserInfo ofNaver(Map<String, Object> attributes) {
-        return OAuth2UserInfo.builder()
-                .provider("naver")
+        return OAuth2UserInfo.builder().provider("naver")
                 .password((String) ((Map) attributes.get("response")).get("id"))
                 .nickname((String) ((Map) attributes.get("response")).get("name"))
-                .email((String) ((Map) attributes.get("response")).get("email"))
-                .build();
+                .email((String) ((Map) attributes.get("response")).get("email")).build();
     }
 
     private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
-        return OAuth2UserInfo.builder()
-                .provider("google")
-                .email((String) attributes.get("email"))
-                .password((String) attributes.get("sub"))
-                .nickname((String) attributes.get("name"))
+        return OAuth2UserInfo.builder().provider("google").email((String) attributes.get("email"))
+                .password((String) attributes.get("sub")).nickname((String) attributes.get("name"))
                 .build();
     }
 
     public Member toEntity() {
-        return Member.builder()
-                .id(UUID.randomUUID())
-                .password(password)
-                .nickname(nickname)
-                .email(email)
-                .is_deleted(false)
-                .build();
+        return Member.builder().id(UUID.randomUUID()).password(password).nickname(nickname)
+                .email(email).isDeleted(false).build();
     }
 }
