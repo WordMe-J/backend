96aec8476eeaf61e8823f72d6d3b82d67db9e1e6
keyy1315 <lovelina1315@gmail.com>
2024-11-22 09:56:31 +0900

    feat : implementation of an email verification process for membership
    이메일 검증을 위한 6자리 코드 전송 및 링크 두 가지 방법으로 임시 구현

--- src/main/java/kr/wordme/controller/TestController.java
+++ src/main/java/kr/wordme/controller/TestController.java
@@ -1,4 +1,30 @@
 package kr.wordme.controller;
 
+import kr.wordme.model.dto.response.VerificationEmailResponseDTO;
+import kr.wordme.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
 public class TestController {
+    private final MemberService memberService;
+
+    @GetMapping("members/verify")
+    public String verificationEmail(@RequestParam("token") String token, @RequestParam("email") String email) {
+        boolean verify = memberService.verificationEmail(token);
+//        VerificationEmailResponseDTO responseDTO = VerificationEmailResponseDTO.create(email, verify);
+        if (verify) {
+            return "redirect:/";
+        }
+            return "redirect:/error.html";
+
+        /**
+         *  1. redirect 방식으로 이메일 검증
+         *  2. 이메일 재전송
+         */
+    }
 }
--- src/main/java/kr/wordme/service/EmailService.java
+++ src/main/java/kr/wordme/service/EmailService.java
@@ -2,6 +2,7 @@ package kr.wordme.service;
 
 
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import kr.wordme.exception.ErrorCode;
 import kr.wordme.exception.member.MemberException;
@@ -9,8 +10,8 @@ import kr.wordme.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Transactional
@@ -22,8 +23,9 @@ public class EmailService {
     private final JwtUtil jwtUtil;
 
     public void sendEmail(String toEmail) throws MessagingException {
+
         String emailToken = jwtUtil.createEmailToken(toEmail);
-        SimpleMailMessage emailForm = createEmailForm(toEmail, emailToken);
+        MimeMessage emailForm = createEmailForm(toEmail, emailToken);
         try {
             mailSender.send(emailForm);
         } catch (MailException e) {
@@ -34,18 +36,31 @@ public class EmailService {
         }
     }
 
-    private SimpleMailMessage createEmailForm(String toEmail, String emailToken) {
+    private MimeMessage createEmailForm(String toEmail, String emailToken) throws MessagingException {
         String body = "<div>"
-                + "<h1> 안녕하세요. Artify 입니다</h1>"
+                + "<h1> 안녕하세요. WordMe 입니다</h1>"
                 + "<br>"
                 + "<p>아래 링크를 클릭하면 이메일 인증이 완료됩니다.<p>"
-                + "<a href='http://localhost:8080/members/verify?token=" + emailToken + "&email=" + toEmail+"'>인증 링크</a>"
+                + "<a href='http://localhost:8080/members/verify?token=" + emailToken + "&email=" + toEmail + "'>인증 링크</a>"
                 + "</div>";
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("WordMe 에서 회원가입 요청을 보냈습니다.");
-        message.setText(body);
+        String code = "123456";
+        String body_code = "<div>"
+                + "<h1> 안녕하세요. WordMe 입니다</h1>"
+                + "<br>"
+                + "<p>회원가입을 위한 이메일 인증 절차입니다.<p>"
+                + "<p>아래의 6자리 인증번호를 이메일 인증 번호 입력란에 입력해주시기 바랍니다.<p>"
+                + "</div>"
+                + "<div style='background: #8e8eff; align-content: center; justify-items: center'>"
+                + "<h2 style='letter-spacing: 10px'>"+code+"</h2>"
+                + "</div>";
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(toEmail);
+        helper.setSubject("WordMe 에서 회원가입 요청을 보냈습니다.");
+        helper.setText(body_code, true);
         return message;
     }
 }
