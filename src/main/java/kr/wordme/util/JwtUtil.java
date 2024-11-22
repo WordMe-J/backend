701c781bfcaf2958642ce13a000756d80814dfda
keyy1315 <lovelina1315@gmail.com>
2024-11-22 16:25:55 +0900

    comment : delete unused import statements

--- src/main/java/kr/wordme/util/JwtUtil.java
+++ src/main/java/kr/wordme/util/JwtUtil.java
@@ -1,18 +1,15 @@
 package kr.wordme.util;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import kr.wordme.model.dto.JwtDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import java.util.Base64;
 import java.util.Date;
