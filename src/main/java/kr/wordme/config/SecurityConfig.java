c26f322b6c3fced60d7f705eb30fb0d2fd79037f
keyy1315 <lovelina1315@gmail.com>
2024-11-22 16:27:01 +0900

    feat : split existing jwtFilter into two steps
    쿠키에 토큰이 담겨있는지 확인하는 필터, 토큰이 유효한지 검증하는 필터 두 단계로 분할하여 구현

--- src/main/java/kr/wordme/config/SecurityConfig.java
+++ src/main/java/kr/wordme/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package kr.wordme.config;
 
 import kr.wordme.filter.JwtFilter;
+import kr.wordme.filter.JwtPresenceFilter;
+import kr.wordme.filter.JwtValidationFilter;
 import kr.wordme.repository.MemberRepository;
 import kr.wordme.service.OAuth2Service;
 import kr.wordme.util.OAuth2SuccessHandler;
@@ -23,42 +25,43 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-        private final MemberRepository memberRepository;
-        private final OAuth2Service oauth2UserService;
-        private final JwtFilter jwtFilter;
-        private final OAuth2SuccessHandler loginSuccessHandler;
+    private final OAuth2Service oauth2UserService;
+    private final JwtFilter jwtFilter;
+    private final OAuth2SuccessHandler loginSuccessHandler;
+    private final JwtPresenceFilter jwtPresenceFilter;
+    private final JwtValidationFilter jwtValidationFilter;
 
-        @Bean
-        public WebSecurityCustomizer webSecurityCustomizer() {
-                return web -> web.ignoring().requestMatchers("/error", "/favicon.ico");
-        }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/error", "/favicon.ico");
+    }
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-                http.csrf(AbstractHttpConfigurer::disable)
-                                .httpBasic(AbstractHttpConfigurer::disable)
-                                .formLogin(config -> config.successHandler(loginSuccessHandler)
-                                                .permitAll())
-                                .logout(AbstractHttpConfigurer::disable)
-                                .sessionManagement(sm -> sm.sessionCreationPolicy(
-                                                SessionCreationPolicy.STATELESS))
-                                .authorizeHttpRequests(req -> req.requestMatchers(
-                                                // new AntPathRequestMatcher("/","/login"),
-                                                // new AntPathRequestMatcher("/members/*")
-                                                "/", "/members/*", "/**")
-                                                .permitAll().anyRequest().authenticated())
-                                .addFilterBefore(jwtFilter,
-                                                UsernamePasswordAuthenticationFilter.class)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(config -> config.successHandler(loginSuccessHandler)
+                        .permitAll())
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req.requestMatchers(
+                                // new AntPathRequestMatcher("/","/login"),
+                                // new AntPathRequestMatcher("/members/*")
+                                "/", "/members/*", "/**")
+                        .permitAll().anyRequest().authenticated())
+                .addFilterBefore(jwtPresenceFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jwtValidationFilter, JwtPresenceFilter.class)
 
+                .headers(headerConfig -> headerConfig.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .oauth2Login(oauth2 -> oauth2.loginPage("/").userInfoEndpoint(
+                                userInfoEndpointConfig -> userInfoEndpointConfig
+                                        .userService(oauth2UserService))
+                        .successHandler(loginSuccessHandler))
 
-                                .headers(headerConfig -> headerConfig.frameOptions(
-                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                                .oauth2Login(oauth2 -> oauth2.loginPage("/").userInfoEndpoint(
-                                                userInfoEndpointConfig -> userInfoEndpointConfig
-                                                                .userService(oauth2UserService))
-                                                .successHandler(loginSuccessHandler))
-
-                ;
-                return http.build();
-        }
+        ;
+        return http.build();
+    }
 }
---
+++ src/main/java/kr/wordme/filter/JwtPresenceFilter.java
@@ -0,0 +1,53 @@
+package kr.wordme.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.wordme.model.dto.JwtDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 쿠키에 토큰이 존재하는지 검증하는 필터
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtPresenceFilter extends OncePerRequestFilter {
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String REFRESH_TOKEN = "refresh_token";
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        JwtDTO tokens = resolveTokenFromRequest(request);
+        if(ObjectUtils.isEmpty(tokens)) {
+            response.sendRedirect("/");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+    private JwtDTO resolveTokenFromRequest(HttpServletRequest request) {
+        Map<String, String> tokens = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        if (!ObjectUtils.isEmpty(cookies)) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(ACCESS_TOKEN)) {
+                    tokens.put(ACCESS_TOKEN, cookie.getValue());
+                }
+                if (cookie.getName().equals(REFRESH_TOKEN)) {
+                    tokens.put(REFRESH_TOKEN, cookie.getValue());
+                }
+            }
+            JwtDTO jwtDTO = JwtDTO.create(tokens);
+            if (!ObjectUtils.isEmpty(tokens)) {
+                return jwtDTO;
+            }
+        }
+        return null;
+    }
+}
